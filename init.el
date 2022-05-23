bf0ed763fc1fbbfbe5e9146d00fb7560bef1551a
lukhen <lukasz.hen@gmail.com>
2022-05-23 07:57:07 +0200

    Initial commit.

---
+++ init.el
@@ -0,0 +1,18 @@
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+(setq package-enable-at-startup nil)
+(straight-use-package 'use-package)
+(straight-use-package '(org :type git :repo "https://git.sr.ht/~bzg/org-contrib"))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
